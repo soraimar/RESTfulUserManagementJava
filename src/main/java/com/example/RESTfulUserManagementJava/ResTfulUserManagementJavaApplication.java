bc0e49df91bf563369df5ac71bbd4c5944887c42
sbernal <soraimar.bernal@gmail.com>
2024-04-10 15:50:51 -0400

    first commit

---
+++ src/main/java/com/example/RESTfulUserManagementJava/ResTfulUserManagementJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulUserManagementJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulUserManagementJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulUserManagementJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RESTfulUserManagementJava/ResTfulUserManagementJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulUserManagementJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulUserManagementJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
